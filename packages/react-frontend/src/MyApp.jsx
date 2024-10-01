22a6dd9970f46a34e4bc4c571f401422b3c9ffdf
The-Last-Kodiak <theseventhkodiak@gmail.com>
2024-10-01 11:05:00 -0700

    finished

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,8 +1,10 @@
 // src/MyApp.jsx
 import React, { useState } from "react";
 import Table from "./Table";
+import Form from "./Form";
 
-const characters = [
+function MyApp() {
+  const [characters, setCharacters] = useState([
   {
     name: "Charlie",
     job: "Janitor"
@@ -19,28 +21,10 @@ const characters = [
     name: "Dennis",
     job: "Bartender"
   }
-];
-
-function MyApp() {
-  const [characters, setCharacters] = useState([
-    {
-      name: "Charlie",
-      job: "Janitor" // the rest of the data
-    },
-    {
-      name: "Mac",
-      job: "Bouncer"
-    },
-    {
-      name: "Dee",
-      job: "Aspring actress"
-    },
-    {
-      name: "Dennis",
-      job: "Bartender"
-    }
-  ]);
-
+    ]);
+  function updateList(person) {
+     setCharacters([...characters, person]);
+}
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
       return i !== index;
@@ -48,13 +32,14 @@ function MyApp() {
     setCharacters(updated);
   }
 	return (
-  	  <div className="container">
-    	    <Table
-      	      characterData={characters}
-      	      removeCharacter={removeOneCharacter}
-    	    />
-  	  </div>
-	);
+  <div className="container">
+    <Table
+      characterData={characters}
+      removeCharacter={removeOneCharacter}
+    />
+    <Form handleSubmit={updateList} />
+  </div>
+   );
 }
 
 export default MyApp;
